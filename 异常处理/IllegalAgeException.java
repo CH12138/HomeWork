8146d61e520e994a24d693e9e23c390c93f23efc
CH12138 <15855900018@163.com>
2020-05-15 11:10:11 +0800

    homework

---
+++ 异常处理/IllegalAgeException.java
@@ -0,0 +1,42 @@
+package com.yunsi.day17.exception06;
+
+public class IllegalAgeException extends Exception{
+	
+	private String errorMsg;  //错误消息 
+	private String errorCode; //错误码 
+	
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+
+
+	public IllegalAgeException(String message) {
+		super(message);
+	}
+	
+	public IllegalAgeException(String message,String errorCode) {
+		super(message);
+		//给本类的属性赋值
+		this.errorMsg=message;
+		this.errorCode=errorCode;
+	}
+}
